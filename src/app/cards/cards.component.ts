bf02735fd9244f265fcfd40c797cd615e5271c44
Brandon Roberts <brandonlroberts@outlook.com>
2019-08-26 22:29:37 -0400

    update edit button.  works but need edit wired.

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Card } from '../entities/card';
 import { CardService } from '../services/card/card.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
-
+  closeResult: string;
   cards: any;
 
-  constructor(private cardService: CardService) { }
+  constructor(private cardService: CardService, private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  edit(card) {
+
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 
   ngOnInit() {
     this.cardService.getCards()
